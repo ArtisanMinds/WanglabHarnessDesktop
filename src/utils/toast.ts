dsh-tauri-desk/deepseek-harness-desktop

refactor(pet): raw session forwarding with drag webm and hook-based webview
- bridge: merge set_pet_activity/set_pet_sessions into push_pet_session and
  forward raw session lifecycle via session:* events; drop activity/session
  projection types, add maid-drag.webm builtin asset, min window width for
  toast rows
- plugin: replace session-status/activity reducers with transferable forwarder;
  push raw DSH session snapshots, pet owns visual arbitration
- webview: rewrite pet window into app + components/pet with use-bubble/
  use-drag/use-pet hooks; remove arbiter/state/drag-machine/session-bubbles
- toast: support live content updates through toast.update event bus

src/utils/toast.ts
@@ -1,10 +1,17 @@
 import type { ToastContentValue } from '@heroui/react/toast'
 import type { ToastVariants } from '@heroui/styles'
 import type { ReactNode } from 'react'
+import { emitter } from '@hairy/react-lib'
 import { ToastQueue } from '@heroui/react'
 
 /** toast() 可选项：库内未暴露的 HeroUIToastOptions（toast-queue 收敛的 content + 超时回调），这里用公开的 ToastContentValue 组合 */
 export type ToastOptions = Partial<ToastContentValue & { timeout?: number, onClose?: () => void }> & { placement?: Placement }
+export type ToastUpdateOptions = Partial<ToastContentValue>
+
+export interface ToastUpdateEvent {
+  key: string
+  options: ToastUpdateOptions
+}
 
 export type Placement = NonNullable<ToastVariants['placement']>
 export const placements = [
@@ -20,33 +27,52 @@ export const queues = Object.fromEntries(
   placements.map(p => [p, new ToastQueue({ maxVisibleToasts: 3 })]),
 ) as Record<Placement, ToastQueue>
 
+const toastContents = new Map<string, ToastContentValue>()
 const placementsKeys = new Map<string, Placement>()
 
-export function toast(
-  message: string | ReactNode,
-  options?: ToastOptions,
-) {
-  // 默认右下角；个别调用方需要其他位置时显式传 placement
-  const { placement = 'bottom end', timeout, onClose, ...rest } = options || {}
-  const key = queues[placement].add(
-    { title: message, ...rest },
-    { timeout, onClose },
-  )
-  placementsKeys.set(key, placement)
-  return key
-}
+/**
+ * 统一 toast API：直接调用创建，toast.update/close/clear 通过 key 管理。
+ * update 触发 emitter 'toast.update'，由 ToastProvider 经 useEventBus 消费后
+ * 原地更新对应 queue 的 content（HeroUI ToastQueue 没有 update 方法）。
+ */
+export const toast = Object.assign(
+  (message: string | ReactNode, options?: ToastOptions) => {
+    // 默认右下角；个别调用方需要其他位置时显式传 placement
+    const { placement = 'bottom end', timeout, onClose, ...rest } = options || {}
+    let key = ''
+    const content = { title: message, ...rest }
+    key = queues[placement].add(content, {
+      timeout,
+      onClose: () => {
+        toastContents.delete(key)
+        placementsKeys.delete(key)
+        onClose?.()
+      },
+    })
+    toastContents.set(key, content)
+    placementsKeys.set(key, placement)
+    return key
+  },
+  {
+    update(key: string, options: ToastUpdateOptions): void {
+      if (!placementsKeys.has(key))
+        return
+      toastContents.set(key, { ...(toastContents.get(key) ?? {}), ...options })
+      emitter.emit('toast.update', { key, options })
+    },
 
-function close(key: string) {
-  const placement = placementsKeys.get(key)
-  placementsKeys.delete(key)
-  if (placement)
-    queues[placement].close(key)
-}
+    close(key: string): void {
+      const placement = placementsKeys.get(key)
+      if (placement)
+        queues[placement].close(key)
+      else
+        toastContents.delete(key)
+    },
 
-function clear() {
-  placementsKeys.clear()
-  placements.forEach(p => queues[p].clear())
-}
-
-toast.close = close
-toast.clear = clear
+    clear(): void {
+      toastContents.clear()
+      placementsKeys.clear()
+      placements.forEach(p => queues[p].clear())
+    },
+  },
+)

ArtisanMinds/WanglabHarnessDesktop

src/utils/toast.ts
@@ -1,10 +1,17 @@
 import type { ToastContentValue } from '@heroui/react/toast'
 import type { ToastVariants } from '@heroui/styles'
 import type { ReactNode } from 'react'
+import { emitter } from '@hairy/react-lib'
 import { ToastQueue } from '@heroui/react'
 
 /** toast() 可选项：库内未暴露的 HeroUIToastOptions（toast-queue 收敛的 content + 超时回调），这里用公开的 ToastContentValue 组合 */
 export type ToastOptions = Partial<ToastContentValue & { timeout?: number, onClose?: () => void }> & { placement?: Placement }
+export type ToastUpdateOptions = Partial<ToastContentValue>
+
+export interface ToastUpdateEvent {
+  key: string
+  options: ToastUpdateOptions
+}
 
 export type Placement = NonNullable<ToastVariants['placement']>
 export const placements = [
@@ -28,33 +35,52 @@ export const activeQueues = navigator.platform.toLowerCase().includes('linux')
     ? linuxQueues
     : queues
 
+const toastContents = new Map<string, ToastContentValue>()
 const placementsKeys = new Map<string, Placement>()
 
-export function toast(
-  message: string | ReactNode,
-  options?: ToastOptions,
-) {
-  // 默认右下角；个别调用方需要其他位置时显式传 placement
-  const { placement = 'bottom end', timeout, onClose, ...rest } = options || {}
-  const key = activeQueues[placement].add(
-    { title: message, ...rest },
-    { timeout, onClose },
-  )
-  placementsKeys.set(key, placement)
-  return key
-}
+/**
+ * 统一 toast API：直接调用创建，toast.update/close/clear 通过 key 管理。
+ * update 触发 emitter 'toast.update'，由 ToastProvider 经 useEventBus 消费后
+ * 原地更新对应 queue 的 content（HeroUI ToastQueue 没有 update 方法）。
+ */
+export const toast = Object.assign(
+  (message: string | ReactNode, options?: ToastOptions) => {
+    // 默认右下角；个别调用方需要其他位置时显式传 placement
+    const { placement = 'bottom end', timeout, onClose, ...rest } = options || {}
+    let key = ''
+    const content = { title: message, ...rest }
+    key = activeQueues[placement].add(content, {
+      timeout,
+      onClose: () => {
+        toastContents.delete(key)
+        placementsKeys.delete(key)
+        onClose?.()
+      },
+    })
+    toastContents.set(key, content)
+    placementsKeys.set(key, placement)
+    return key
+  },
+  {
+    update(key: string, options: ToastUpdateOptions): void {
+      if (!placementsKeys.has(key))
+        return
+      toastContents.set(key, { ...(toastContents.get(key) ?? {}), ...options })
+      emitter.emit('toast.update', { key, options })
+    },
 
-function close(key: string) {
-  const placement = placementsKeys.get(key)
-  placementsKeys.delete(key)
-  if (placement)
-    activeQueues[placement].close(key)
-}
+    close(key: string): void {
+      const placement = placementsKeys.get(key)
+      if (placement)
+        activeQueues[placement].close(key)
+      else
+        toastContents.delete(key)
+    },
 
-function clear() {
-  placementsKeys.clear()
-  placements.forEach(p => activeQueues[p].clear())
-}
-
-toast.close = close
-toast.clear = clear
+    clear(): void {
+      toastContents.clear()
+      placementsKeys.clear()
+      placements.forEach(p => activeQueues[p].clear())
+    },
+  },
+)